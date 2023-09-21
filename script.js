7c91edd5ce765a6b560782050a5b50f1c576be64
Rodrigo-Sousa1 <rodrigo.sousaa.lima@gmail.com>
2023-09-21 19:08:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let imagen = document.querySelector("img")
+ 
+function apagar() {  
+    imagen.src= "https://mariaccarolina.github.io/L-mpada/img/lamp.jpg" 
+}
+
+console.log(apagar)
+
+
+
+function acender() { 
+    imagen.src ="https://mariaccarolina.github.io/L-mpada/img/acesa.jpg"} 
+
+console.log(acender)
